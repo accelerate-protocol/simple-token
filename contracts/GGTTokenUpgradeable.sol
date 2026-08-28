8e8c8d4409625ee6300b702d74aa494c989a62e8
Joseph C Wang <joequant@gmail.com>
2026-08-28 12:13:44 +0800

    fix up the contract

--- contracts/GGTTokenUpgradeable.sol
+++ contracts/GGTTokenUpgradeable.sol
@@ -2,24 +2,34 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
-contract GGTTokenUpgradeable is Initializable, UUPSUpgradeable, OwnableUpgradeable, ERC20Upgradeable {
+contract GGTTokenUpgradeable is Initializable, UUPSUpgradeable, ERC20Upgradeable, AccessControlUpgradeable {
+    bytes32 public constant MINT_ROLE = keccak256("MINT_ROLE");
+    bytes32 public constant BURN_ROLE = keccak256("BURN_ROLE");
+    bytes32 public constant UPGRADE_ROLE = keccak256("UPGRADE_ROLE");
+
     function initialize(string memory name_, string memory symbol_, uint256 initialSupply_) public initializer {
         __ERC20_init(name_, symbol_);
-        __Ownable_init(msg.sender);
+        __AccessControl_init();
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(UPGRADE_ROLE, msg.sender);
         _mint(msg.sender, initialSupply_);
     }
 
-    function mint(address to, uint256 amount) public onlyOwner {
+    function mint(address to, uint256 amount) external onlyRole(MINT_ROLE) {
         _mint(to, amount);
     }
 
-    function burn(address from, uint256 amount) public onlyOwner {
+    function burn(address from, uint256 amount) external onlyRole(BURN_ROLE) {
         _burn(from, amount);
     }
 
-    function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
+    function upgrade(address newImplementation) external onlyRole(UPGRADE_ROLE) {
+        upgradeToAndCall(newImplementation, "");
+    }
+
+    function _authorizeUpgrade(address newImplementation) internal override onlyRole(UPGRADE_ROLE) {}
 }
